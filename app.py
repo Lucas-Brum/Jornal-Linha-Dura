c3e62d443703dc57314330f7f4a18b7cefcfc87e
lucas.brum <lucas.brum@VISONET.LOCAL>
2025-11-12 17:52:49 -0300

    Fazendo requests

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-# /d:/Documents/pessoal/Jornal-Linha-Dura/main.py
+import json
 from src.get_news import NewsAPI
 
 if __name__ == "__main__":
     news_api = NewsAPI()
+    news = news_api.get_top_stories(limit=1)
+    print(json.dumps(news, indent=2, ensure_ascii=False))
